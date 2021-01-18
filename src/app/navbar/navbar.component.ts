c0640e162384363c9c5e126dc65dccf3de04f6e4
Sufyan Shaikh <absufyn@gmail.com>
2021-01-18 22:12:02 +0530

    Header/Footer Development.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  public showHideNavigationMobile = false;
+
+  public showHideHome = false;
+  public showHidePages = false;
+  public showService = false;
+  public showDoctor = false;
+  public showBlog = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line: typedef
+  public toggleNavigationMobile() {
+    this.showHideNavigationMobile = !this.showHideNavigationMobile;
+  }
+
+  // tslint:disable-next-line: typedef
+  public toggleHomeOption() {
+    this.showHideHome = !this.showHideHome;
+  }
+
+  // tslint:disable-next-line: typedef
+  public togglePages() {
+    this.showHidePages = !this.showHidePages;
+  }
+
+  // tslint:disable-next-line: typedef
+  public toggleService() {
+    this.showService = !this.showService;
+  }
+
+  // tslint:disable-next-line: typedef
+  public toggleDoctor() {
+    this.showDoctor = !this.showDoctor;
+  }
+
+  // tslint:disable-next-line: typedef
+  public toggleBlog() {
+    this.showBlog = !this.showBlog;
+  }
 }
